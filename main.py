e90df33c69920a60bd0f4ce7a5a9b89a7e5fa7bc
Omar <omarkhaled1752003@yahoo.com>
2023-09-04 09:26:04 +0300

    added calculate button, and the button command

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import tkinter as tk
 
+# Button Command
+def to_km():
+    miles = int(miles_entry.get())
+    km_entry.config(textvariable=miles)
+
+
 # Window Setup
 window = tk.Tk()
 window.minsize(width=400, height=150)
 window.title("Mile to Km")
 
 # Entry
-miles = tk.Entry()
-miles.grid(column=5, row=2)
-miles.config(width=10)
+miles_entry = tk.Entry()
+miles_entry.grid(column=5, row=2)
+miles_entry.config(width=10)
 
-km = tk.Entry()
-km.grid(column=5, row=3)
-km.config(width=10)
+km_entry = tk.Entry()
+km_entry.grid(column=5, row=3)
+km_entry.config(width=10, state="readonly")
 
 # Labels
 tk.Label(text="Miles").grid(column=6, row=2)
 tk.Label(text="is equal to").grid(column=4, row=3)
 tk.Label(text="Km").grid(column=6, row=3)
+
+# Button
+tk.Button(text="Calculate", command=to_km).grid(column=5, row=4)
 
 window.mainloop()
